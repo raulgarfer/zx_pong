94791c7d40a7dce694ddf0ece006b77d527bbff1
raul garcia <raulgarfer@gmail.com>
2023-10-12 11:57:58 +0200

    barras laterales

--- pong/paso1/main.asm
+++ pong/paso1/main.asm
@@ -1,11 +1,20 @@
 org $8000
-    ld hl,$4000
-    ld b,$c0
+    ld hl,$4000         ;;HL= scanline 9,lindea 0,tercio 0 columna 0
+    ld b,$c0            ;;B=192, scanlines que tiene la pantalla
 loop:
-    ld (hl),$3c
+    ld (hl),$3c         ;;pinta en la pantalla 001111000
     call nextscan
     ;;halt  ;;quitar para ver como se pinta lentamente
     djnz loop
+    
+    ld hl,$57ff
+    ld b,$c0 
+
+loop_up:
+    ld (hl),$3c
+    call previous_scan
+    ;;halt
+    djnz loop_up
 ret
 include "video.asm"
 end $8000
--- pong/paso1/video.asm
+++ pong/paso1/video.asm
@@ -25,3 +25,28 @@ nextscan:
     sub $08
     ld h,a 
 ret
+;;WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW
+;;previous scan
+;;obtiene la posicion de memoria correspondiente al scanline
+;;anterior al inducado.
+;;010T TSSS LLLC CCCC
+;;entrasa: HL como scanline actual
+;;salida: HL como scanline siguiente
+;;altera AF,HL
+;;MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+previous_scan:
+    ld a,h 
+    dec h 
+    and $07
+        ret nz 
+;;calcula la linea anterior
+    ld a,l 
+    sub $20
+    ld l,a 
+        ret c 
+;;si llega aquim ha pasasdo al scamline 7 de la linea anterior
+;;y ha restado un tercio, que volvemos a sumar
+    ld a,h 
+    add a,$08
+    ld h,a 
+ret
